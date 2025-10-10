9a5d987272ec7e66e744e6eb33b9a87c1c0f6f3a
brycen-q-hang <daoquanghang.hue@gmail.com>
2025-10-10 23:15:33 +0700

    49

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,6 +1,7 @@
 package vn.hoidanit.laptopshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +18,10 @@ public class UserController {
     }
 
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
         String text = this.userService.handerHello();
+        model.addAttribute("eric", text);
+        model.addAttribute("hoidanit", "from controller with model");
         return "hello";
     }
 }
